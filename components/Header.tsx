e1e58827ac2e30117e2ee75c2652819148b17354
Von <xvtran@hotmail.com>
2023-02-24 11:49:14 -0800

    header finished

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,28 @@
 import { BellIcon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -14,7 +15,7 @@ const Home: NextPage = () => {
       <Header />
 
       <main>
-        {/* Banner */}
+        <Banner />
         <section>
           {/* Row */}
           {/* Row */}
